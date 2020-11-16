df885ecf1e3936d748acb7edfb299a10f83e9743
xd59 <xd59@njit.edu>
2020-11-16 00:18:30 -0500

    add ajax login

--- auth/login.php
+++ auth/login.php
@@ -1,37 +1,24 @@
 <?php require '../inc/conn.php' ?>
 
 <?php
-session_start();
-if(isset($_SESSION['user'])) {
-    unset($_SESSION['user']);
-}
+$name = $_GET['n'];
+$pass = hash("sha512", $_GET['p']);
 
-echo "<h1> log in </h1><br>";
-
-// Get info from html
-$username = $_POST['username'];
-$password = hash("sha512", $_POST['password']);
-
-// check database
-$sql = "select * from myblog where username='$username'";
+$sql = "select * from myblog where username='$name'";
 $query = mysqli_query($conn, $sql);
 
 if(mysqli_num_rows($query) > 0) {
-        $row = mysqli_fetch_assoc($query);
+    $row = mysqli_fetch_assoc($query);
         
-        if($row['password'] == $password) { // log in successful
-            $_SESSION['user'] = $row['username'];
-            header("Location: ../post/postList_page.php");
-        }
-        else {
-                echo "password not right.<br>";
-        }    
+    if($row['password'] == $pass) { // log in successful
+        echo true;
+    }
+    else {
+        echo false;
+    }    
 }
 else {
-        echo "no such user.<br>";        
+    echo false;        
 }
-
 ?>
-
-<a href="login.html">back</a>
 
